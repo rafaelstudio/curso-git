4ca2be4341c8d6661df3d7a950116613cfe9d062
studiosafeway@gmail.com <studiosaferay@gmail.com>
2017-10-14 15:20:58 -0300

    mensagem adicionada

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -7,6 +7,8 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia ");
+            Console.WriteLine("Como vc esta");
 
             Console.ReadLine();
         }
